6f311a6827cbd424f2640d292a0ce68e65eebcd1
Gokhan Kurt <gokhan0kurt@gmail.com>
2023-05-19 14:18:10 +0300

    priority_example was added

---
+++ src/test/java/CG01_Priority.java
@@ -0,0 +1,27 @@
+import org.testng.annotations.Test;
+
+public class CG01_Priority {
+
+    //  TestNG testleri harf sirasina gore calistirir (ONGORULEBILIR)
+
+    //  priority sayisal olarak kucukten buyuge dogru testleri calistirir
+    //  eger priority degeri ayni olan birden fazla test varsa, yine harf sirasina bakar
+
+
+
+    @Test(priority = -4)
+    public void testYoutube(){
+        System.out.println("youtube");
+    }
+
+
+    @Test
+    public void testShopify(){
+        System.out.println("shopify");
+    }
+
+    @Test(priority = 1)
+    public void testAmazon(){
+        System.out.println("amazon");
+    }
+}
