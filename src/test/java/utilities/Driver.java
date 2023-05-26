46f4cd3ace288c5c14366764e303f278ad091758
Gokhan Kurt <gokhan0kurt@gmail.com>
2023-05-26 12:00:46 +0300

    Driver class'a son sekli verildi

--- src/test/java/utilities/Driver.java
+++ src/test/java/utilities/Driver.java
@@ -3,27 +3,61 @@ package utilities;
 import io.github.bonigarcia.wdm.WebDriverManager;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.edge.EdgeDriver;
+import org.openqa.selenium.firefox.FirefoxDriver;
 
 import java.time.Duration;
 
 public class Driver {
 
-    static WebDriver driver;
+    private static WebDriver driver;  // obje olusturulamasin diye Access Modifier degistirildi
 
-    public static WebDriver getDriver(){
-        WebDriverManager.chromedriver().setup();
+    private Driver(){   // Singleton Pattern
 
-        if (driver==null){
-           driver =new ChromeDriver();
-           driver.manage().window().maximize();
-           driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(15));
-       }
+    }
+
+    public static WebDriver getDriver() {
+
+        String istenenBrowser = ConfigReader.getProperty("browser");
+
+        if (driver == null) {
+
+            switch (istenenBrowser) {
+
+                case "firefox":
+                    WebDriverManager.firefoxdriver().setup();
+                    driver = new FirefoxDriver();
+                    break;
+                case "edge":
+                    WebDriverManager.edgedriver().setup();
+                    driver = new EdgeDriver();
+                    break;
+                default:
+                    WebDriverManager.chromedriver().setup();
+                    driver = new ChromeDriver();
+
+            }
+
+            driver.manage().window().maximize();
+            driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(15));
+        }
 
         return driver;
     }
 
-    public static void closeDriver(){
-        driver.close();
+    public static void closeDriver() {
+        if (driver != null){
+            driver.close();
+            driver = null;
+        }
+
+    }
+
+    public static void quitDriver() {
+        if (driver != null){
+            driver.quit();
+            driver = null;
+        }
     }
 
 
